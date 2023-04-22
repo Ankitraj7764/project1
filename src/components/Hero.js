ad62defa1c32a3bbedd7fc313ea6edfed84d951e
Ankitraj7764 <ankitgupta56518@gmail.com>
2023-04-22 16:33:19 +0530

    responsive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,21 +2,27 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <>
-            <div class="hero flex flex-col md:flex-row m-4">
-                <div class="heroleft flex-1 w-full md:w-56 m-20">
-                    <div class="">
-                        <h1 class="text-[#2F281E] font-['Montserrat'] text-4xl lg:text-[74px] font-bold leading-[100px]">Make </h1>
-                        <h1 class="text-[#2F281E] font-['Montserrat'] text-4xl lg:text-[74px] font-bold leading-[100px]">learning fun!</h1>
-                        <p className="text-[#2F281E] font-['Montserrat'] text-4xl md:text-[24px] font-bold leading-[100px]">Any subject, in any language, on any device, for all ages!</p>
-                    </div>
-                    <button className="bg-[#3A5AFF] w-96 h-12 rounded-md font-['Montserrat'] text-white font-bold">Sign up for free</button>
-                </div>
-                <div class="heroright flex-1 w-full md:w-52  md:my-20">
-                    <img class="" src={require("./image/hero.jpg")} alt="" />
-                </div>
+        <><div className="hero flex 2xl:m-20  sm:m-1">
+        <div className="heroleft flex-1 flex-wrap md:w-1/2 m-2 md:m-2  2xl:mr-36 ">
+          <div className="2xl:text-[74px]">
+            <div className="flex font-['Montserrat'] ">
+            <h1 className="text-[#2F281E] font-[1000] 2xl:text-[74px]  md:text-6xl leading-tight font-montserrat 2xl:my-4">Make <h1> learning fun!</h1></h1>
             </div>
-
+            <p className="text-[#2F281E]  text-2xl md:text-xl font-montserrat leading-tight mt-4 2xl:my-4">Any subject, in any language, on any device, for all ages!</p>
+          
+          </div>
+         
+          <button className="bg-[#3A5AFF] w-full md:w-72 h-12 rounded-md font-bold text-white font-montserrat mt-8 md:mt-16 2xl:my-4 2xl:w-64">Sign up for free</button>
+          <p className="font-['Montserrat'] my-8 text-[18px] font-[1000] ">Or download the app:</p>
+         <div className="flex ">
+                <img className="pr-4" src={require("./image/app.png")} alt="app" />
+                <img src={require("./image/google.png")} alt="google" />
+            </div> </div>
+        <div className="heroright flex-1   md:w-1/2 md:my-0">
+          <img className="w-full" src={require("./image/hero.jpg")} alt="" />
+        </div>
+      </div>
+      
         </>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,26 +5,26 @@ const Navbar = () => {
     
     return (
         <>
-            <div className="navbar mt-4 mb-800 pr-4 py-0 flex">
+            <div className="navbar mt-4 mb-800 pr-4 py-0 flex flex-wrap ">
                 <div className="heading flex-1">
                   <div className="flex">
                   <img className=" pl-8 h-12 my-auto mr" src={require("./image/Logo.png")} alt="" />
-                       <h1 className="w-[351px] h-[51px] top-[43px] left-[132px] font-['vibur']
+                       <h1 className=" text-[50px] w-[351px] h-[51px] top-[43px] left-[132px] font-['Vibur']
                        ">Kurtis Classrom</h1>  
                   </div>
                    
                 </div>
-                <ul className="flex-1 flex-row-reverse mx-4">
-                    <div className="flex flex-row-reverse">
-                        <li className=" border-2 rounded-sm w-16 h-8 mx-4"><a href="#news" className="flex align-middle justify-center py-1">News</a></li>
-                        <li className=" p-2 "><a href="#news">News</a></li>
+                <ul className="flex-1 flex-row-reverse mx-4 ">
+                    <div className="flex flex-row-reverse text-xl">
+                        <li className=" p-2 "><a href="#news"><img src={require("./image/group16.png")} alt="" /></a></li>
                      
-                            <li class="bg-sky-600 rounded-md text-white w-16 mx-4 h-8 "><a href="#home" className="flex align-middle justify-center py-1">Home</a></li>
+                        <li className=" border-2 rounded-sm w-24 h-10 mx-4 p-1 "><a href="#news" className="flex align-middle justify-center ">News</a></li>
+                            <li className="bg-[#3A5AFF] rounded-md text-white w-24 h-10 mx-4 p-1"><a href="#home" className="flex align-middle justify-center py-1">Home</a></li>
                         
 
-                        <li class="dropdown p-2">
-                            <a href="#" class="dropbtn">Dropdown</a>
-                            <div class="dropdown-content">
+                        <li className="dropdown p-2">
+                            <a href="#" className="dropbtn">Dropdown</a>
+                            <div className="dropdown-content">
 
                             </div>
                         </li>
